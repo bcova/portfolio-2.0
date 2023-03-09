fd78dbda4c806850e433942dba9ef00bcba29347
bcova <brandoncova@gmail.com>
2023-03-09 16:31:08 -0600

    Testing

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import selfie from "../assets/navbarSelfie.jpeg";
 export default function Navbar() {
   return (
-    <div className="w-screen h-screen grid grid-rows-6 grid-cols-6">
+    <div className="w-screen h-screen grid grid-rows-6 grid-cols-6"> 
         <div className="bg-red-600 col-span-full row-span-1">Header</div>
         {/* <div className="bg-green-600 row-span-4">2</div> */}
         <div className="bg-yellow-600 col-span-full row-span-4">
