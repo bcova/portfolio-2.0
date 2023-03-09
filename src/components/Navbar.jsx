a92d38f43d2a0c01ceb5bf9c4fed37e45d166d09
bcova <brandoncova@gmail.com>
2023-03-09 17:12:04 -0600

    fixing grid

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import selfie from "../assets/navbarSelfie.jpeg";
 export default function Navbar() {
   return (
-    <div className="w-screen h-screen grid grid-rows-6 grid-cols-6"> 
-        <div className="bg-red-600 col-span-full row-span-1">Header</div>
-        {/* <div className="bg-green-600 row-span-4">2</div> */}
-        <div className="bg-yellow-600 col-span-full row-span-4">
+    <div className="w-screen h-screen grid grid-rows-8 grid-cols-6 grid-flow-col-dense"> 
+        <div className="bg-red-600 col-span-full">Header</div>
+
+        <div className="bg-yellow-600 col-span-full row-[span_6_/_span_6]">
             <div className="bg-purple-600 h-full w-full overflow-scroll overflow-x-hidden snap-y snap-mandatory flex flex-col items-center">
                     <div className="bg-green-600 w-full h-full rounded-lg flex-shrink-0 snap-start snap-always">
                                   Main
@@ -18,8 +18,8 @@ export default function Navbar() {
                     </div>
               </div>
         </div>
-        {/* <div className="bg-orange-600 row-span-4">4</div> */}
-        <div className="bg-blue-600 col-span-full row-span-1">Footer</div>
+
+        <div className="bg-blue-600 col-span-full row-span-1 ">Footer</div>
     </div>
   );
 }
